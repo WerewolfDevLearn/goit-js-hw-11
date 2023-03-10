82b71f19ad3bcc3d7e1b241baacde5aae9d37bd4
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2023-03-11 00:59:40 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,83 @@
 import { getImages } from './js/fetchGallery';
 import { imageCardTmpl } from './js/markupTemplates';
+import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const buttonLM = document.querySelector('[data-action]');
-form.submit.addEventListener('click', onSubmit);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+const state = {
+  arrOfImages: [],
+  page: 1,
+  totalPics: 0,
+  keyword: '',
+};
 
-function onSubmit(event) {
+form.submit.addEventListener('click', onSubmit);
+buttonLM.addEventListener('click', loadMore);
+
+async function onSubmit(event) {
   event.preventDefault();
-  getImages('dog', 1).then(hits => injectGalleryItem(hits));
+  if (!form.query.value) {
+    Notify.info('Please enter keyword!');
+    return;
+  }
+  state.keyword = form.query.value;
+  console.log(state);
+  const data = await getImages(state.keyword, state.page);
+  setState(data);
+  console.log(state);
+  Notify.info(`Hooray! We found ${state.totalPics} images.`);
+  gallery.innerHTML = '';
+  injectGalleryItem(state.arrOfImages);
+  toggleButton();
+}
+async function loadMore() {
+  const data = await getImages(state.keyword, state.page);
+  setState(data);
+  injectGalleryItem(state.arrOfImages);
+  console.log(state);
+  toggleButton();
+}
+
+function setState(data) {
+  if (data.hits.length === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  state.arrOfImages = [];
+  state.arrOfImages = [...data.hits];
+  state.totalPics = data.totalHits;
+  state.page = state.page + 1;
+}
+
+function resetState() {
+  state.arrOfImages = [];
+  state.page = 1;
+  state.totalPics = 0;
 }
 
 function injectGalleryItem(arr) {
   const markup = arr.map(item => imageCardTmpl(item)).join('');
-
   gallery.insertAdjacentHTML('beforeend', markup);
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  lightbox.refresh();
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: 'smooth',
   });
-  console.log(lightbox);
-  buttonLM.classList.toggle('button-hidden');
+}
+function toggleButton() {
+  if (state.page > 1 && state.page * 40 < state.totalPics) {
+    buttonLM.classList.remove('button-hidden');
+  } else {
+    buttonLM.classList.add('button-hidden');
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
 }
--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import key from './key';
+import { Notify } from 'notiflix';
 
-const URL = `https://pixabay.com/api/?key=${key}&`;
-
-axios.defaults.baseURL = URL;
-
-export function getImages(string, page) {
-  return axios
-    .get(
-      `q=${string}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-    )
-    .then(({ data }) => data.hits);
+export async function getImages(string, page) {
+  try {
+    const respons = await axios.get(
+      `https://pixabay.com/api/?key=${key}&q=${string}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    );
+    const data = await respons.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
